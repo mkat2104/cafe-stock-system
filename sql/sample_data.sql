d43edfeeecfb95896b978516b53b94be133ec351
akmmyat <myta1477@gmail.com>
2025-11-25 22:28:35 +0700

    Added sample data file

---
+++ sql/sample_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO cafes (name, address)
+VALUES
+    ('Sunrise Cafe', 'Bangkok, Thailand'),
+    ('Aconut', 'Bangkok, Thailand'),
+    ('Daily Brew', 'Bangkok, Thailand');
+
+
+INSERT INTO ingredients (name, stock, unit)
+VALUES
+    ('Coffee Beans', 50, 'kg'),
+    ('Milk', 30, 'liters'),
+    ('Sugar', 100, 'kg'),
+    ('Chocolate Syrup', 20, 'bottles'),
+    ('Matcha Powder', 15, 'kg');
+
+
+INSERT INTO menu_items (name, price)
+VALUES
+    ('Hot Latte', 60.00),
+    ('Iced Latte', 70.00),
+    ('Hot Americano', 55.00),
+    ('Matcha Latte', 85.00),
+    ('Mocha', 75.00);
